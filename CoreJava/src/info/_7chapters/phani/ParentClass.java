9596f7b188761217ed5e96353c94a25fb9a22d12
7chapters <jayram.rout@7chapters.info>
2013-02-22 20:14:11 -0800

    Core Java

---
+++ CoreJava/src/info/_7chapters/phani/ParentClass.java
@@ -0,0 +1,16 @@
+package com.excel.phani;
+
+public class ParentClass {
+	
+	int assetValue = 1000;
+	public ParentClass(){
+		System.out.println("ParentClass.ParentClass()============");
+	}
+	public ParentClass(int assetValue){
+		this.assetValue = assetValue;
+	}
+	
+	protected void callSuper(){
+		System.out.println("Call Super.....");
+	}
+}
---
+++ CoreJava/src/info/_7chapters/phani/ThisExample.java
@@ -0,0 +1,13 @@
+package com.excel.phani;
+
+public class ThisExample {
+	
+	
+	public static void main(String[] args) {
+		MyTesting test = new MyTesting();
+		test.thisExample();
+		
+		MyTesting test1 = new MyTesting("Jayram");
+	}
+
+}
